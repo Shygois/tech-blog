d5c7f3130f536e5407b23d067e48ede90d79f1d9
Shygois <shayana.gois@att.net>
2021-05-07 21:13:03 -0700

    setting up project

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
