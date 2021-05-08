10fd9ef2daf89c2a63ae4d09564736903cf24e6a
Shygois <shayana.gois@att.net>
2021-05-07 21:28:52 -0700

    adding prebuilt css file

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
