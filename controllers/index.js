85e4e0aa88bca08a2b14530d70c851c8ef514c9c
Shygois <shayana.gois@att.net>
2021-05-20 19:21:33 -0700

    updated index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,15 @@
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
 const router = require('express').Router();
 
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
